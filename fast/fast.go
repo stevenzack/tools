85fa13e87d2cfcf65859490be89b427a775c8188
stevenzack <stevenzack@qq.com>
2019-02-19 17:25:27 +0800

    Just a Backup

--- fast/fast.go
+++ fast/fast.go
@@ -1,10 +1,21 @@
 package fast
 
-import "github.com/valyala/fasthttp"
+import (
+	"github.com/StevenZack/tools/fast/views"
+	"github.com/valyala/fasthttp"
+)
 
 func GetHeader(cx *fasthttp.RequestCtx, s string) string {
 	return string(cx.Request.Header.Peek(s))
 }
 func GetURI(cx *fasthttp.RequestCtx) string {
 	return string(cx.URI().Path())
+}
+func NotFound(cx *fasthttp.RequestCtx) {
+	cx.Response.SetStatusCode(404)
+	SetHeaderHTML(cx)
+	cx.WriteString(views.Str_404)
+}
+func SetHeaderHTML(cx *fasthttp.RequestCtx) {
+	cx.Response.Header.Set("Content-Type", "text/html")
 }
