f4ca72aa01bb6e67915ed94d82105c619e3f46c1
stevenzack <zhouyuhao@xiaoheiban.cn>
2019-08-23 21:40:03 +0800

    Just a Backup

--- encodingToolkit/encoding_test.go
+++
@@ -1,7 +0,0 @@
-package encodingToolkit
-
-import "testing"
-
-func Test_UrlEncode(t *testing.T) {
-	t.Log(UrlDecode(`%E5%8E%8B%E7%BC%A9%E5%90%8E%E7%9A%84root%E7%9B%AE%E5%BD%95.zip`))
-}
--- fileToolkit/pkg_test.go
+++
@@ -1,8 +0,0 @@
-package fileToolkit
-
-import "testing"
-
-func Test_a(t *testing.T) {
-	dir := GetPathOfGoPkg("/Users/stevenzacker/go/src/github.com/StevenZack/gengo/example/data", "../model")
-	t.Log(GetGoPkgFromDirPath(dir))
-}
