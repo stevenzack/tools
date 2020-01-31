797162f421e73c4fbd389b28d434bcb860cfc5bd
stevenzack <zhouyuhao@xiaoheiban.cn>
2020-01-31 23:09:11 +0800

    mac

--- osToolkit/font.go
+++ osToolkit/font.go
@@ -2,17 +2,25 @@ package osToolkit
 
 import "runtime"
 
-var windowsChineseFonts = map[string]struct{}{
-	"msyh.ttc":    struct{}{},
-	"simhei.ttf":  struct{}{},
-	"simli.ttf":   struct{}{},
-	"simyou.ttf":  struct{}{},
-	"simkai.ttf":  struct{}{},
-	"simfang.ttf": struct{}{},
-}
+var (
+	darwinChineseFonts = map[string]struct{}{
+		"STHeiti Light.ttc":  struct{}{},
+		"STHeiti Medium.ttc": struct{}{},
+	}
+	windowsChineseFonts = map[string]struct{}{
+		"msyh.ttc":    struct{}{},
+		"simhei.ttf":  struct{}{},
+		"simli.ttf":   struct{}{},
+		"simyou.ttf":  struct{}{},
+		"simkai.ttf":  struct{}{},
+		"simfang.ttf": struct{}{},
+	}
+)
 
 func GetSystemChineseFont() string {
 	switch runtime.GOOS {
+	case "darwin":
+		return "/System/Library/Fonts/STHeiti Light.ttc"
 	default:
 		return "C:\\Windows\\Fonts\\msyh.ttc"
 	}
