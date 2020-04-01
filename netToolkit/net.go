6dcc683ceaa297ba5487eeffdcdb6d6ee41680c9
stevenzack <zigzigcheers@gmail.com>
2020-04-01 22:27:55 +0800

    Just a Backup

--- netToolkit/net.go
+++ netToolkit/net.go
@@ -247,6 +247,7 @@ func DownloadFile(url, fdist string) error {
 		return e
 	}
 	defer rp.Body.Close()
+	os.MkdirAll(strToolkit.SubBeforeLast(fdist, string(os.PathSeparator), fdist), 0755)
 	f, e := os.OpenFile(fdist, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
 	if e != nil {
 		return e
