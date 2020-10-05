33401f01439eeb9d419d6535852106abf1db5632
stevenzack <zigzigcheers@gmail.com>
2020-10-05 10:27:05 +0800

    Just a Backup

--- compressToolkit/zip.go
+++ compressToolkit/zip.go
@@ -11,6 +11,136 @@ import (
 	"github.com/StevenZack/tools/strToolkit"
 )
 
+func CompressFilesTo(dst io.Writer, paths []string, progress func(offset, total int64) error) error {
+	const bufSize = 32 << 10
+	var total, offset int64
+	// total
+	for _, path := range paths {
+		info, e := os.Stat(path)
+		if e != nil {
+			return e
+		}
+		if info.IsDir() {
+			e = filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+				if !info.IsDir() {
+					total += info.Size()
+				}
+				return nil
+			})
+			if e != nil {
+				log.Println(e)
+				return e
+			}
+		} else {
+			total += info.Size()
+		}
+	}
+
+	// write
+	zw := zip.NewWriter(dst)
+	defer zw.Close()
+	for _, root := range paths {
+		info, e := os.Stat(root)
+		if e != nil {
+			return e
+		}
+		base := filepath.Dir(root)
+		if info.IsDir() {
+			e = filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+				rel, e := filepath.Rel(base, path)
+				if e != nil {
+					log.Println(e)
+					return e
+				}
+				if info.IsDir() {
+					_, e := zw.Create(rel)
+					if e != nil {
+						log.Println(e)
+						return e
+					}
+					return nil
+				}
+				header, e := zip.FileInfoHeader(info)
+				if e != nil {
+					log.Println(e)
+					return e
+				}
+				header.Name = rel
+
+				writer, e := zw.CreateHeader(header)
+				if e != nil {
+					log.Println(e)
+					return e
+				}
+				fi, e := os.OpenFile(path, os.O_RDONLY, 0644)
+				if e != nil {
+					log.Println(e)
+					return e
+				}
+				defer fi.Close()
+				buf := make([]byte, bufSize)
+				for {
+					n, e := fi.Read(buf)
+					if e != nil {
+						log.Println(e)
+						return e
+					}
+					_, e = writer.Write(buf[:n])
+					if e != nil {
+						log.Println(e)
+						return e
+					}
+					offset += int64(n)
+					if progress != nil {
+						progress(offset, total)
+					}
+				}
+			})
+			if e != nil {
+				log.Println(e)
+				return e
+			}
+			continue
+		}
+		// is file
+		header, e := zip.FileInfoHeader(info)
+		if e != nil {
+			log.Println(e)
+			return e
+		}
+		writer, e := zw.CreateHeader(header)
+		if e != nil {
+			log.Println(e)
+			return e
+		}
+		fi, e := os.OpenFile(root, os.O_RDONLY, 0644)
+		if e != nil {
+			log.Println(e)
+			return e
+		}
+		defer fi.Close()
+		buf := make([]byte, bufSize)
+		for {
+			n, e := fi.Read(buf)
+			if e != nil {
+				log.Println(e)
+				return e
+			}
+			_, e = writer.Write(buf[:n])
+			if e != nil {
+				log.Println(e)
+				return e
+			}
+			offset += int64(n)
+			if progress != nil {
+				progress(offset, total)
+			}
+		}
+	}
+	progress(total, total)
+	return nil
+}
+
 func CompressFileTo(dst io.Writer, path string, progress func(offset, total int64)) error {
 	const bufSize = 32 * 1024
 	var total int64
@@ -117,7 +247,7 @@ func CompressFileTo(dst io.Writer, path string, progress func(offset, total int6
 }
 
 //解压
-func DeCompress(zipFile, dest string) error {
+func Decompress(zipFile, dest string) error {
 	reader, e := zip.OpenReader(zipFile)
 	if e != nil {
 		log.Println(e)
