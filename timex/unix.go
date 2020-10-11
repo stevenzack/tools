a9144fb47b721f0159af2282f7a8fba03ccb774f
stevenzack <zigzigcheers@gmail.com>
2020-10-11 13:51:30 +0800

    unix

---
+++ timex/unix.go
@@ -0,0 +1,29 @@
+package timex
+
+import (
+	"strconv"
+	"time"
+)
+
+func ValidateUnix(i int64) bool {
+	if i > 1000000000 && i < 1900000000 {
+		return true
+	}
+	return false
+}
+
+func NowUnixTime() int64 {
+	return UnixTimeOf(time.Now())
+}
+func UnixTimeOf(t time.Time) int64 {
+	i := t.Unix()
+	if ValidateUnix(i) {
+		return i
+	}
+	s := strconv.FormatInt(i, 64)
+	if len(s) < 10 {
+		return i
+	}
+	it, _ := strconv.ParseInt(s[:10], 10, 64)
+	return it
+}
