066a524f5d6b0fee4600a672c0d683410b6b78bc
Steven Zack <zigzigcheers@gmail.com>
2021-09-17 10:54:48 +0800

    ResponseWriterBuffer

--- ioToolkit/responsewriter.go
+++ ioToolkit/responsewriter.go
@@ -1,37 +1,56 @@
 package ioToolkit
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"net"
 	"net/http"
-	"strings"
+	"reflect"
 )
 
-type ResponseWriterLogger struct {
-	builder strings.Builder
-	w       http.ResponseWriter
+type ResponseWriterBuffer struct {
+	buffer     *bytes.Buffer
+	w          http.ResponseWriter
+	statusCode int
 }
 
-func NewResponseWriterLogger(w http.ResponseWriter) *ResponseWriterLogger {
-	return &ResponseWriterLogger{
-		w: w,
+func NewResponseWriterLogger(w http.ResponseWriter) *ResponseWriterBuffer {
+	return &ResponseWriterBuffer{
+		buffer: new(bytes.Buffer),
+		w:      w,
 	}
 }
 
-func (r *ResponseWriterLogger) Header() http.Header {
+func (r *ResponseWriterBuffer) Header() http.Header {
 	return r.w.Header()
 }
 
-func (r *ResponseWriterLogger) Write(b []byte) (int, error) {
-	return r.builder.Write(b)
+func (r *ResponseWriterBuffer) Write(b []byte) (int, error) {
+	return r.buffer.Write(b)
 }
 
-func (r *ResponseWriterLogger) WriteHeader(statusCode int) {
-	r.w.WriteHeader(statusCode)
+func (r *ResponseWriterBuffer) WriteHeader(statusCode int) {
+	r.statusCode = statusCode
 }
 
-func (r *ResponseWriterLogger) String() string {
-	return r.builder.String()
+func (r *ResponseWriterBuffer) String() string {
+	return r.buffer.String()
 }
 
-func (r *ResponseWriterLogger) Flush() {
-	r.w.Write([]byte(r.builder.String()))
+func (r *ResponseWriterBuffer) Bytes() []byte {
+	return r.buffer.Bytes()
+}
+
+func (r *ResponseWriterBuffer) Flush() {
+	r.w.WriteHeader(r.statusCode)
+	r.w.Write(r.Bytes())
+}
+
+func (r *ResponseWriterBuffer) Hijack() (rwc net.Conn, buf *bufio.ReadWriter, err error) {
+	h, ok := r.w.(http.Hijacker)
+	if ok {
+		return h.Hijack()
+	}
+	panic(fmt.Sprintf("ResponseWriterBuffer.w:%s is not a http.Hijack", reflect.TypeOf(r.w).String()))
 }
