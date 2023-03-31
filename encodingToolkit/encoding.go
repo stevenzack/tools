1b918e87b7a9f883315fe371fbbf9684efb5d819
Steven Zack <zigzigcheers@gmail.com>
2023-03-31 08:58:07 +0800

    remove-golang-x-text

--- encodingToolkit/encoding.go
+++ encodingToolkit/encoding.go
@@ -1,23 +1,10 @@
 package encodingToolkit
 
 import (
-	"net/url"
-	"bytes"
 	"encoding/json"
-	"io/ioutil"
-	"runtime"
-
-	"golang.org/x/text/encoding/simplifiedchinese"
-	"golang.org/x/text/transform"
+	"net/url"
 )
 
-func EncodingGBK(src string) string {
-	if runtime.GOOS != "windows" {
-		return src
-	}
-	data, _ := ioutil.ReadAll(transform.NewReader(bytes.NewReader([]byte(src)), simplifiedchinese.GBK.NewEncoder()))
-	return string(data)
-}
 func JsonObj(i interface{}) string {
 	b, e := json.Marshal(i)
 	if e != nil {
@@ -32,9 +19,9 @@ func JsonArray(i interface{}) string {
 	}
 	return string(b)
 }
-func UrlEncode(s string)string{
+func UrlEncode(s string) string {
 	return url.QueryEscape(s)
 }
-func UrlDecode(s string)(string,error){
+func UrlDecode(s string) (string, error) {
 	return url.QueryUnescape(s)
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/StevenZack/tools
 
 go 1.15
-
-require golang.org/x/text v0.3.5
